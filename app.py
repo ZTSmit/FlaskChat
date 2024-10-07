6cfc283324ba8237c6c6f3c4df5c0d6d6b792104
ZTSmit <zedtosu@gmail.com>
2024-10-07 15:07:19 +0700

    Added .gitignore, modified app.py, added chat.html
    OpenAI API works, environment set up, frontend
    not yet responsive

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+client = OpenAI()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, Flask!"
+    #return render_template('chat.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
